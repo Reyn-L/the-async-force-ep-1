c4f1fb3cf1f19f50a01ad02877978e30e06370df
Reyn-L <Reyn.Leong@gmail.com>
2017-07-07 19:31:54 -1000

    darth vader and tatooine

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 /*jshint esversion: 6 */
 var oReq = new XMLHttpRequest();
-function reqListener() {
-  console.log(this.responseText);
+function name4Person() {
+  let obj = JSON.parse(this.responseText);
+  document.getElementById('person4Name').innerHTML = obj.name;
 }
-oReq.addEventListener('load', reqListener);
+oReq.addEventListener('load', name4Person);
 oReq.open("get", "http://swapi.co/api/people/4/");
 oReq.send();
-console.log(this.name);
 
-let person4 = document.getElementById(person4Name);
-
-console.log(person4);
+var oReq2 = new XMLHttpRequest();
+function planet1() {
+  let obj2 = JSON.parse(this.responseText);
+  document.getElementById('person4HomeWorld').innerHTML = obj2.name;
+}
+oReq2.addEventListener('load', planet1);
+oReq2.open("get", "http://swapi.co/api/planets/1/");
+oReq2.send();
