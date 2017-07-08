a7812a358cdb09cd8b25cc0b8812403a77be9ec4
Reyn-L <Reyn.Leong@gmail.com>
2017-07-07 19:47:00 -1000

    han solo human

--- js/app.js
+++ js/app.js
@@ -16,3 +16,21 @@ function planet1() {
 oReq2.addEventListener('load', planet1);
 oReq2.open("get", "http://swapi.co/api/planets/1/");
 oReq2.send();
+
+var oReq3 = new XMLHttpRequest();
+function name14Person() {
+  let obj3 = JSON.parse(this.responseText);
+  document.getElementById('person14Name').innerHTML = obj3.name;
+}
+oReq3.addEventListener('load', name14Person);
+oReq3.open("get", "http://swapi.co/api/people/14/");
+oReq3.send();
+
+var oReq4 = new XMLHttpRequest();
+function planet22() {
+  let obj4 = JSON.parse(this.responseText);
+  document.getElementById('person14Species').innerHTML = obj4.name;
+}
+oReq4.addEventListener('load', planet22);
+oReq4.open("get", "http://swapi.co/api/species/1");
+oReq4.send();
